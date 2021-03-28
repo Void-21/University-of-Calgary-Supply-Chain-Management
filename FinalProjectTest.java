80b8b469762f1b3c39de991ed83c6a2fd5a32b51
Mahtab Khan <mahtabukhan16@gmail.com>
2021-03-28 17:49:58 +0400

    Update FinalProjectTest.java

--- FinalProjectTest.java
+++ FinalProjectTest.java
@@ -1,4 +1,4 @@
-package edu.ucalgary.ensf409;
+//package edu.ucalgary.ensf409;
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.io.*;
@@ -39,27 +39,96 @@ public class FinalProjectTest {
         FinalProject testobj = new FinalProject(DBURL,USERNAME,PASSWORD);
         String [][] testArray = new String [5][7];
 
-        testArray[0][0] = 'F002';
-        testArray[0][1] = 'F007';
-        testArray[0][2] = 'F008';
-        testArray[0][3] = 'F009';
-        testArray[0][4] = 'F014';
+        // testArray[0][0] = "F002";
+        // testArray[0][1] = "Medium";
+        // testArray[0][2] = "N";
+        // testArray[0][3] = "N";
+        // testArray[0][4] = "Y";
+        // testArray[0][5] = "100";
+        // testArray[0][6] = "004";
 
-        testArray[1][0] = 'Medium';
-        testArray[1][1] = 'Medium';
-        testArray[1][2] = 'Medium';
-        testArray[1][3] = 'Medium';
-        testArray[1][4] = 'Medium';
+        // testArray[1][0] = "F007";
+        // testArray[1][1] = "Medium";
+        // testArray[1][2] = "N";
+        // testArray[1][3] = "Y";
+        // testArray[1][4] = "Y";
+        // testArray[1][5] = "150";
+        // testArray[1][6] = "002";
 
-        testArray[1][0] = '';
-        testArray[1][1] = '';
-        testArray[1][2] = 'Medium';
-        testArray[1][3] = 'Medium';
-        testArray[1][4] = 'Medium';
+        // testArray[2][0] = "F008";
+        // testArray[2][1] = "Medium";
+        // testArray[2][2] = "Y";
+        // testArray[2][3] = "N";
+        // testArray[2][4] = "N";
+        // testArray[2][5] = "50";
+        // testArray[2][6] = "005";
 
+        // testArray[3][0] = "F009";
+        // testArray[3][1] = "Medium";
+        // testArray[3][2] = "Y";
+        // testArray[3][3] = "Y";
+        // testArray[3][4] = "N";
+        // testArray[3][5] = "150";
+        // testArray[3][6] = "004";
 
+        // testArray[2][0] = "F014";
+        // testArray[2][1] = "Medium";
+        // testArray[2][2] = "Y";
+        // testArray[2][3] = "Y";
+        // testArray[2][4] = "Y";
+        // testArray[2][5] = "200";
+        // testArray[2][6] = "002";
+        testArray = arrayFromFile(filing,5,7);
+        Boolean flag = false;
 
+        String [][] fromFunc = testobj.filingSelect("Medium", 5);
+        if (! checkIfSame(fromFunc, testArray))
+        {
+            System.out.println("Output from filingSelect is wrong!");
+        }
     }
+
+    public boolean checkIfSame(String [][] arr1, String [][] arr2)
+    {
+        int rows = arr1.length;
+        int col = arr1[0].length;
+        Boolean flag = false;
+
+        for(int i=0; i < rows ; i++)
+        {
+            for( int j=0 ; j < col ; j++)
+            {
+                if( arr1[i][j].equals(arr2[i][j]))
+                {
+                    flag = true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+        return flag;
+    }
+
+    public String [][] arrayFromFile(String filename, int rows, int columns)
+    {
+       Scanner sc = new Scanner(new BufferedReader(new FileReader( filename + ".txt")));
+      // int rows = 5;
+      // int columns = 7;
+      String [][] myArray = new String[rows][columns];
+      while(sc.hasNextLine()) {
+         for (int i=0; i<myArray.length; i++) {
+            String[] line = sc.nextLine().trim().split(",");
+            for (int j=0; j<line.length; j++) {
+               myArray[i][j] = (line[j]);
+            }
+         }
+      }
+      return myArray;
+    }
+
     @Test
     public void testCheckFiling() {
         FinalProject testobj = new FinalProject(DBURL,USERNAME,PASSWORD);
@@ -119,4 +188,5 @@ public class FinalProjectTest {
     public void testWriteOrderForm(){
         FinalProject testobj = new FinalProject(DBURL,USERNAME,PASSWORD);
     }
+
 }
